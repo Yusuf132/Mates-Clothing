5f3b7f889955ad8415f2fa7f91a81e83f325774a
Yousuff khan <youskhan@cisco.com>
2020-04-20 17:35:48 +0530

    added collection preview and shop components

--- src/App.js
+++ src/App.js
@@ -2,21 +2,16 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import {Switch, Route} from 'react-router-dom';
+import Shop from './pages/shop/shop.component';
 
-const HATS =()=> {
-  return(
-    <div>
-      <h1>HATS</h1>
-    </div>
-  )
-}
+
 
 function App() {
   return (
     <div>
     <Switch>
       <Route  exact path='/' component= {HomePage}/>
-      <Route  path='/shop/hats' component= {HATS}/>
+      <Route  path='/shop' component= {Shop}/>
     </Switch>
     </div>
   );
