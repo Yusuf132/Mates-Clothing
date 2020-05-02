917ec5a64adc6462e6d48125f480276be132c924
Yousuff khan <youskhan@cisco.com>
2020-05-02 15:01:56 +0530

    add styled component for custom button

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './custom-button.styles.scss';
+import {CustomButtonContainer} from './custom-button.styles';
 
-const CustomButton = ({type,children,isGoogleSignIn,inverted, ...otherProps}) => (
-    <button className={`
-        ${inverted? 'inverted': ''}
-        ${isGoogleSignIn? 'google-sign-in': ''} custom-button`} 
-        type={type} {...otherProps}>
+const CustomButton = ({children, ...props}) => (
+    <CustomButtonContainer {...props}>
     {children}
-    </button>
+    </CustomButtonContainer>
 )
 
 export default CustomButton;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,7 +7,7 @@ import CartIcon from '../cart-icon/cart-icon.component';
 import {createStructuredSelector} from 'reselect';
 import {selectCurrentUser} from '../../redux/user/user.selectors';
 import {selectCartHidden} from '../../redux/cart/cart.selectors';
-import {HeaderContainer, LogoContainer, OptionsContainer, OptionLink, OptionDiv} from './header.styles';
+import {HeaderContainer, LogoContainer, OptionsContainer, OptionLink} from './header.styles';
 
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
@@ -21,7 +21,7 @@ const Header = ({currentUser, hidden}) => (
             <OptionLink to= '/contact' >CONTACT</OptionLink>
             {
                 currentUser?
-                (<OptionDiv  onClick={()=> auth.signOut()}>SIGN OUT</OptionDiv>):
+                (<OptionLink as='div' onClick={()=> auth.signOut()}>SIGN OUT</OptionLink>):
                 (<OptionLink to= '/signin' >SIGN IN</OptionLink>)
             }
             <CartIcon />
